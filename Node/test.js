56d6529aa17675a950532c90ae11943312d71acd
n-hari159 <n.hari159@gmail.com>
2019-02-13 15:34:41 +0530

    New folder added

--- Node/test.js
+++ Node/test.js
@@ -2,7 +2,7 @@ var http = require("http");
 
 http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'});
-   console.log("abc");
+   console.log("abc");      //Double Console-log Output
    response.end('Hello World\n');
 }).listen(8081);
 
