2cffd9d25a21c7b317738dd4bfe00545d428208b
n-hari159 <n.hari159@gmail.com>
2019-02-28 14:57:16 +0530

    age calculator sample app

--- class/agecalculator.js
+++ class/agecalculator.js
@@ -1,20 +1,22 @@
 function ageCal() {
-    var birth_Date = document.getElementById('birth_date').value;
-    var d = new Date(birth_Date);
-    var b_date = birth_Date.toString();
-    var year = parseInt(b_date.substring(0, 4), 10);
-    var month = parseInt(b_date.substring(5, 7), 10);
-    var day = parseInt(b_date.substring(8, 10), 10);
+    var birthDate = document.getElementById('birth_date').value;
+    var d = new Date(birthDate);
+    var mdate = birthDate.toString();
+    var yearThen = parseInt(mdate.substring(0, 4), 10);
+    var monthThen = parseInt(mdate.substring(5, 7), 10);
+    var dayThen = parseInt(mdate.substring(8, 10), 10);
     var today = new Date();
-    var birthday = new Date(year, month - 1, day);
+    var birthday = new Date(yearThen, monthThen - 1, dayThen);
     var differenceInMilisecond = today.valueOf() - birthday.valueOf();
+    var year_age = Math.floor(differenceInMilisecond / 31584000000);
+    var day_age = Math.floor((differenceInMilisecond % 31584000000) / 86400000);
 
-    var fyear = Math.floor(differenceInMilisecond / 31536000000);
+    var month_age = Math.floor(day_age / 30);
+    day_age = day_age % 30;
 
-    var fday = Math.floor((differenceInMilisecond % 31536000000) / 86400000);
-    fday = fday % 30;
-
-    var fmonth = Math.floor(fday / 30);
-
-    document.getElementById("age").innerHTML = fyear + " years " + fmonth + " months " + fday + " days";
+    if (isNaN(year_age) || isNaN(month_age) || isNaN(day_age)) {
+        document.getElementById("age").innerHTML = "Please Enter your Date of Birth, Darling !";
+    } else {
+        document.getElementById("age").innerHTML = year_age + " years " + month_age + " months " + day_age + " days";
+    }
 }
